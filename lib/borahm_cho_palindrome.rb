f3a5b7ab214863f61ace04476de7ac6c4d553d86
Borahm Cho <borahm@Borahms-MacBook-Pro.local>
2020-09-24 09:15:03 -0400

    Finish working and refactored palindrome method

--- lib/borahm_cho_palindrome.rb
+++ lib/borahm_cho_palindrome.rb
@@ -1,6 +1,16 @@
 require "borahm_cho_palindrome/version"
 
-module BorahmChoPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
