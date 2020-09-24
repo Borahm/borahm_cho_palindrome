b6d8383128953de1ca1e4a0573e0cf2bed309599
Borahm Cho <borahm@Borahms-MacBook-Pro.local>
2020-09-24 00:25:16 -0400

    Initialize repository

---
+++ lib/borahm_cho_palindrome.rb
@@ -0,0 +1,6 @@
+require "borahm_cho_palindrome/version"
+
+module BorahmChoPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/borahm_cho_palindrome/version.rb
@@ -0,0 +1,3 @@
+module BorahmChoPalindrome
+  VERSION = "0.1.0"
+end
