46bc178995bf46f290d0c6ecd293bed4c1d100dc
Borahm Cho <borahm@borahm.de>
2020-09-29 09:27:36 -0400

    Updated Gem

--- lib/borahm_cho_palindrome.rb
+++ lib/borahm_cho_palindrome.rb
@@ -1,21 +1,24 @@
 require "borahm_cho_palindrome/version"
 
-class String
+module BorahmChoPalindrome 
 
-  # Returns true for a palindrome, false otherwise.
-  def palindrome?
-    processed_content == processed_content.reverse
-  end
-
-  # Returns the letters in the string.
-  def letters
-    self.chars.select { |c| c.match(/[a-z]/i) }.join
-  end
-
-  private
-
-    # Returns content for palindrome testing.
-    def processed_content
-      self.letters.downcase
+    # Returns true for a palindrome, false otherwise.
+    def palindrome?
+      processed_content == processed_content.reverse
     end
-end
+  
+    private
+  
+      # Returns content for palindrome testing.
+      def processed_content
+        to_s.scan(/[a-zA-Z\w]/i).join.downcase
+      end
+  end
+  
+  class String
+    include BorahmChoPalindrome
+  end
+
+  class Integer
+    include BorahmChoPalindrome
+  end
